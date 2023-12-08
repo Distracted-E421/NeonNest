98409b750de7e8c84f1ffb4858ff3eba00f6b8d4
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-08 14:12:35 -0600

    .

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import csv
+import pandas as pd
 
 # Example of reading the products.csv file
 with open('products.csv', mode='r', encoding='utf-8') as file:
     csv_reader = csv.reader(file)
     header = next(csv_reader)  # Gets the header row
     products = list(csv_reader)  # Reads the rest of the data
+
+products_df = pd.read_csv('products.csv')
