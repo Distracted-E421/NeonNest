80abb0fb893bf9811041f69303134f2803c2a200
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 16:22:29 -0600

    Update input_validation.py

--- src/utils/input_validation.py
+++ src/utils/input_validation.py
@@ -24,3 +24,22 @@ def validate_date_format(date):
         return True
     else:
         return False
+    
+def validate_phone_number_format(phone):
+    phone_regex = r'^\d{3}-\d{3}-\d{4}$'
+    if re.match(phone_regex, phone):
+        return True
+    else:
+        return False
+
+def validate_first_name(first_name):
+    if validate_string_input(first_name):
+        if first_name[0].isupper() and first_name.isalpha():
+            return True
+    return False
+
+def validate_last_name(last_name):
+    if validate_string_input(last_name):
+        if last_name.replace("-", "").isalpha():
+            return True
+    return False
