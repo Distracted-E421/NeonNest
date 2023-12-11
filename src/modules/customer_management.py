0375df3142c57be7bd18bfe22cb12b372c4e735e
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 17:33:39 -0600

    Update customer_management.py

--- src/modules/customer_management.py
+++ src/modules/customer_management.py
@@ -1,50 +1,76 @@
-from ..database.db_operations import *
-from ..utils.input_validation import *
+
+from database.db_operations import *
+from utils.encryption_util import *
 
 def add_new_customer():
-    customer_first_name = input("Enter customer first name: ")
-    customer_last_name = input("Enter customer last name: ")
-    customer_email = input("Enter customer email: ")
-    customer_phone = input("Enter customer phone number: ")
+    first_name = input("Enter customer first name: ")
+    last_name = input("Enter customer last name: ")
+    email = input("Enter customer email: ")
+    phone = input("Enter customer phone number: ")
 
-    validate_input(customer_first_name, customer_last_name, customer_email, customer_phone)
+    encryption_key = generate_key()
+    encrypted_email = encrypt_data(encryption_key, email)
+    encrypted_phone = encrypt_data(encryption_key, phone)
 
-    customer_data = create_customer_data(customer_first_name, customer_last_name, customer_email, customer_phone)
-    save_customer(customer_data)
+    customer_data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": encrypted_email,
+        "phone": encrypted_phone,
+        "encryption_key": encryption_key  # Store this key securely with the customer record
+    }
+    add_customer(customer_data)
+    print("Customer added successfully")
+
+    first_name = input("Enter customer first name: ")
+    last_name = input("Enter customer last name: ")
+    email = input("Enter customer email: ")
+    phone = input("Enter customer phone number: ")
+
+    if not (validate_string_input(first_name) and validate_string_input(last_name)
+            and validate_email_format(email) and validate_phone_number_format(phone)):
+        print("Invalid input")
+        return
+
+    customer_data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "phone": phone
+    }
+    add_customer(customer_data)
     print("Customer added successfully")
 
 def update_customer_info():
-    customer_email = input("Enter customer email: ")
-    customer_phone = input("Enter customer phone number: ")
+    email = input("Enter customer email to update: ")
+    new_phone = input("Enter new customer phone number: ")
 
-    validate_input(None, None, customer_email, customer_phone)
+    if not (validate_email_format(email) and validate_phone_number_format(new_phone)):
+        print("Invalid input")
+        return
 
-    customer_data = create_customer_data(None, None, customer_email, customer_phone)
-    update_customer(customer_data)
+    update_customer(email, new_phone)
     print("Customer information updated successfully")
 
 def search_customer_by_name():
-    customer_name = input("Enter customer name: ")
-    customers = get_all_customers()
+    name = input("Enter customer name to search: ")
+    if not validate_string_input(name):
+        print("Invalid name input")
+        return
 
-    found_customers = []
-    for customer in customers:
-        if customer['first_name'].lower() == customer_name.lower() or customer['last_name'].lower() == customer_name.lower():
-            found_customers.append(customer)
-
-    if len(found_customers) > 0:
+    customers = get_customers(name)
+    if customers:
         print("Found customers:")
-        for customer in found_customers:
-            print(f"Name: {customer['first_name']} {customer['last_name']}, Email: {customer['email']}, Phone: {customer['phone']}")
+        for customer in customers:
+            print(f"{customer['first_name']} {customer['last_name']}, Email: {customer['email']}, Phone: {customer['phone']}")
     else:
         print("No customers found with the given name.")
 
 def list_all_customers():
-    customers = get_all_customers()
-
-    if len(customers) > 0:
+    customers = get_customers()
+    if customers:
         print("All customers:")
         for customer in customers:
-            print(f"Name: {customer['first_name']} {customer['last_name']}, Email: {customer['email']}, Phone: {customer['phone']}")
+            print(f"{customer['first_name']} {customer['last_name']}, Email: {customer['email']}, Phone: {customer['phone']}")
     else:
         print("No customers found.")
