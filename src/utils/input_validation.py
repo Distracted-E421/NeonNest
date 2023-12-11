45434af1af663c89ed17c3cfd30085bf675a7618
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 13:53:05 -0600

    Update input_validation.py

--- src/utils/input_validation.py
+++ src/utils/input_validation.py
@@ -1,7 +1,26 @@
-# input_validation.py
+import re
+def validate_string_input(input_string):
+    if isinstance(input_string, str) and len(input_string) > 0:
+        return True
+    else:
+        return False
 
-def validate_input(input_data, data_type):
-    # Implement validation logic
-    pass
+def validate_email_format(email):
+    email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    if re.match(email_regex, email):
+        return True
+    else:
+        return False
 
-# Add more utility functions
+def validate_integer_input(input_integer):
+    if isinstance(input_integer, int):
+        return True
+    else:
+        return False
+
+def validate_date_format(date):
+    date_regex = r'^\d{4}-\d{2}-\d{2}$'
+    if re.match(date_regex, date):
+        return True
+    else:
+        return False
