b8c8e9c0ed828abd11451ce159f788cacb63d6a3
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 16:22:26 -0600

    Update customer_management.py

--- src/modules/customer_management.py
+++ src/modules/customer_management.py
@@ -2,33 +2,16 @@ from ..database.db_operations import *
 from ..utils.input_validation import *
 
 def add_new_customer():
-    # TODO: Implement add_new_customer function
-    customer_name = input("Enter customer name: ")
+    customer_first_name = input("Enter customer first name: ")
+    customer_last_name = input("Enter customer last name: ")
     customer_email = input("Enter customer email: ")
     customer_phone = input("Enter customer phone number: ")
 
-    # Validate input
-    if not validate_name(customer_name):
-        print("Invalid customer name")
-        return
+    validate_input(customer_first_name, customer_last_name, customer_email, customer_phone)
 
-    if not validate_email(customer_email):
-        print("Invalid customer email")
-        return
-
-    if not validate_phone(customer_phone):
-        print("Invalid customer phone number")
-        return
-
-    # Save customer information to database
-    customer_data = {
-        "name": customer_name,
-        "email": customer_email,
-        "phone": customer_phone
-    }
+    customer_data = create_customer_data(customer_first_name, customer_last_name, customer_email, customer_phone)
     save_customer(customer_data)
     print("Customer added successfully")
-
 
 def update_customer_info():
     # TODO: Implement update_customer_info function
