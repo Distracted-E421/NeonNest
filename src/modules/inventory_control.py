d36058c2ed3806087f8e2fe14ca52f924d4f108b
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 16:53:24 -0600

    Update inventory_control.py

--- src/modules/inventory_control.py
+++ src/modules/inventory_control.py
@@ -21,15 +21,32 @@ def perform_database_operation(operation, *args):
 def add_inventory_item(item):
     # Insert the item into the inventory table
     # ...
-
-def update_inventory(item_id, new_quantity):
-    # Update the quantity of the item in the inventory table
-    # ...
+    # Add your logic here to insert the item into the inventory table
+    # For example:
+    db.execute("INSERT INTO inventory (item_name, quantity) VALUES (?, ?)", (item['name'], item['quantity']))
+    db.commit()
 
 def delete_inventory_item(item_id):
     # Delete the item from the inventory table
     # ...
+    # Add your logic here to delete the item from the inventory table
+    # For example:
+    db.execute("DELETE FROM inventory WHERE item_id = ?", (item_id,))
+    db.commit()
 
 def view_inventory_list():
     # Retrieve the inventory list from the database
     # ...
+    # Add your logic here to retrieve the inventory list from the database
+    # For example:
+    db.execute("SELECT * FROM inventory")
+    inventory_list = db.fetchall()
+    return inventory_list
+
+def update_inventory(item_id, new_quantity):
+    # Update the quantity of the item in the inventory table
+    # ...
+    # Add your logic here to update the quantity of the item in the inventory table
+    # For example:
+    db.execute("UPDATE inventory SET quantity = ? WHERE item_id = ?", (new_quantity, item_id))
+    db.commit()
