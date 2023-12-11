d1d2278c578a8c31645572739f2158a3129f5bf0
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 17:34:15 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-from database import db_connection
-from modules import (
-    db_operations,
-    customer_management,
-    inventory_control,
-    order_processing,
-    input_validation,
-    encryption_util
-)
+# main.py
+from database.db_connection import *
+from modules.customer_management import *
+from modules.inventory_control import *
+from modules.order_processing import *
+from database.db_operations import *
 
 def main_menu():
     print("Welcome to NeonNest!")
@@ -16,34 +13,39 @@ def main_menu():
     print("3. Process Orders")
     print("4. View Products")
     print("5. View Sales Records")
+    print("6. Create New Order")
     print("0. Exit")
 
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        customer_management.manage_customers()
+        manage_customers()
     elif choice == "2":
-        inventory_control.manage_inventory()
+        manage_inventory()
     elif choice == "3":
-        order_processing.process_orders()
-    elif products = get_all_products()
+        process_orders()
+    elif choice == "4":
+        products = get_all_products()
         for product in products:
             print(product)
     elif choice == "5":
         sales_records = get_all_sales()
         for record in sales_records:
             print(record)
+    elif choice == "6":
+        process_new_order()
     elif choice == "0":
         print("Exiting NeonNest. Goodbye!")
-        db_connection.close_connection()
+        close_connection()
         return
     else:
         print("Invalid choice. Please try again.")
+        main_menu()
 
 if __name__ == "__main__":
     try:
-        db_connection.initialize_connection()
+        initialize_connection()
         main_menu()
     except Exception as e:
         print("An error occurred:", str(e))
-        db_connection.close_connection()
+        close_connection()
