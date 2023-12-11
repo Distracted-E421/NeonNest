f203f2841aa04131f49da6323abc3b692d585fd3
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 17:55:54 -0600

    Update db_operations.py

--- src/database/db_operations.py
+++ src/database/db_operations.py
@@ -1,74 +1,49 @@
-from .db_connection import *
+from db_connection import *
 
+# Function to retrieve all customers from the database
 def get_all_customers():
-    # Retrieves all customers from the database.
-    # Returns:
-    #     A list of tuples representing the customers.
-
     conn = create_connection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM customers"
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM customers")
+        results = cursor.fetchall()
     return results
 
+# Function to insert a new customer into the database
 def insert_customer(customer):
-    # Inserts a new customer into the database.
-    # Args:
-    #     customer: A dictionary containing the customer's name and email.
-
     conn = create_connection()
-    cursor = conn.cursor()
-    query = "INSERT INTO customers (name, email) VALUES (?, ?)"
-    cursor.execute(query, (customer['name'], customer['email']))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with conn.cursor() as cursor:
+        query = "INSERT INTO customers (name, email) VALUES (%s, %s)"
+        cursor.execute(query, (customer['name'], customer['email']))
+        conn.commit()
 
+# Function to update an existing customer in the database
 def update_customer(customer):
-    # Updates an existing customer in the database.
-    # Args:
-    #     customer: A dictionary containing the customer's name, email, and id.
-
     conn = create_connection()
-    cursor = conn.cursor()
-    query = "UPDATE customers SET name = ?, email = ? WHERE id = ?"
-    cursor.execute(query, (customer['name'], customer['email'], customer['id']))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with conn.cursor() as cursor:
+        query = "UPDATE customers SET name = %s, email = %s WHERE id = %s"
+        cursor.execute(query, (customer['name'], customer['email'], customer['id']))
+        conn.commit()
 
+# Function to delete a customer from the database
 def delete_customer(customer_id):
-    # Deletes a customer from the database.
-    # Args:
-    #     customer_id: The id of the customer to be deleted.
-
     conn = create_connection()
-    cursor = conn.cursor()
-    query = "DELETE FROM customers WHERE id = ?"
-    cursor.execute(query, (customer_id,))
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with conn.cursor() as cursor:
+        query = "DELETE FROM customers WHERE id = %s"
+        cursor.execute(query, (customer_id,))
+        conn.commit()
 
+# Function to retrieve all products from the database
 def get_all_products():
     conn = create_connection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM products"
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM products")
+        results = cursor.fetchall()
     return results
 
+# Function to retrieve all sales from the database
 def get_all_sales():
     conn = create_connection()
-    cursor = conn.cursor()
-    query = "SELECT * FROM sales"
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    conn.close()
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM sales")
+        results = cursor.fetchall()
     return results
