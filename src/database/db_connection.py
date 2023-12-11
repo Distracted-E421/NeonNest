899d3a36e0586b59adacfe26d321a52b39059242
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 17:34:09 -0600

    Update db_connection.py

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,18 +1,39 @@
 import mysql.connector
 from mysql.connector import errorcode
-from .db_config import DB_CONFIG
+from db_config import DB_CONFIG
 
-def create_connection():
+# Global variable for the database connection
+connection = None
+
+# Function to initialize the database connection
+def initialize_connection():
+    global connection
     try:
-        # Attempt to establish a connection to the database
         connection = mysql.connector.connect(**DB_CONFIG)
-        return connection
+        print("Database connection successfully established.")
     except mysql.connector.Error as err:
-        # Handle specific error cases
-        error_messages = {
-            errorcode.ER_ACCESS_DENIED_ERROR: "Invalid credentials",
-            errorcode.ER_BAD_DB_ERROR: "Database not found"
-        }
-        # Print the error message corresponding to the error code
-        print(error_messages.get(err.errno, err))
-        return None
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Invalid credentials")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database not found")
+        else:
+            print(f"Error connecting to the database: {err}")
+
+# Function to create a new database connection
+def create_connection():
+    return connection
+
+# Function to close the database connection
+def close_connection():
+    global connection
+    if connection and connection.is_connected():
+        connection.close()
+        print("Database connection closed.")
+
+# Function to check if the database connection is active
+def check_connection():
+    global connection
+    if connection and connection.is_connected():
+        return True
+    else:
+        return False
