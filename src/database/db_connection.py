a2cb41334d4c1834d0f183dc6dba225a1af6141c
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 13:52:29 -0600

    Update db_connection.py

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,23 +1,22 @@
-# db_connection.py
-
 import mysql.connector
 from mysql.connector import errorcode
 from .db_config import DB_CONFIG
 
 def create_connection():
-    """
-    Creates a connection to the database using the provided configuration.
+    # Create a connection to the database using the provided configuration
+    connection = mysql.connector.connect(**DB_CONFIG)
+    return connection
 
-    Returns:
-        connection: A connection object if the connection is successful, None otherwise.
-    """
     try:
+        # Attempt to establish a connection to the database
         connection = mysql.connector.connect(**DB_CONFIG)
         return connection
     except mysql.connector.Error as err:
+        # Handle specific error cases
         error_messages = {
             errorcode.ER_ACCESS_DENIED_ERROR: "Invalid credentials",
             errorcode.ER_BAD_DB_ERROR: "Database not found"
         }
+        # Print the error message corresponding to the error code
         print(error_messages.get(err.errno, err))
         return None
