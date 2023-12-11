e5ac58d0220ef2a20160042690bc80f7f41b76ea
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 15:34:06 -0600

    Update load_data_neon_nest_1.sql

--- sql/load_data_neon_nest_1.sql
+++ sql/load_data_neon_nest_1.sql
@@ -1,5 +1,5 @@
 -- Load data into CustomerProfile table
-LOAD DATA LOCAL INFILE '/home/mal/neon-nest/CustomerProfile.csv'
+LOAD DATA LOCAL INFILE '/home/mal/NeonNest/data/csv/CustomerProfile.csv'
 INTO TABLE CustomerProfile
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -7,7 +7,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 -- Load data into ApartmentUnit table
-LOAD DATA LOCAL INFILE '/home/mal/neon-nest/ApartmentUnit.csv'
+LOAD DATA LOCAL INFILE '/home/mal/NeonNest/data/csv/ApartmentUnit.csv'
 INTO TABLE ApartmentUnit
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -15,7 +15,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 -- Load data into Order table
-LOAD DATA LOCAL INFILE '/home/mal/neon-nest/Order.csv'
+LOAD DATA LOCAL INFILE '/home/mal/NeonNest/data/csv/Order.csv'
 INTO TABLE `Order`
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -23,7 +23,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 -- Load data into SupplyItem table
-LOAD DATA LOCAL INFILE '/home/mal/neon-nest/SupplyItem.csv'
+LOAD DATA LOCAL INFILE '/home/mal/NeonNest/data/csv/SupplyItem.csv'
 INTO TABLE SupplyItem
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -31,7 +31,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
 -- Load data into Supplier table
-LOAD DATA LOCAL INFILE '/home/mal/neon-nest/Supplier.csv'
+LOAD DATA LOCAL INFILE '/home/mal/NeonNest/data/csv/Supplier.csv'
 INTO TABLE Supplier
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
