126f836344ae9736d7b4d37a8394627d67564d71
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-11 18:00:10 -0600

    fin

--- src/utils/encryption_util.py
+++ src/utils/encryption_util.py
@@ -1,17 +1,33 @@
 from cryptography.fernet import Fernet
 
-# Generate a key for encryption and decryption
 def generate_key():
+    """Generates a key for encryption and decryption using Fernet."""
     return Fernet.generate_key()
 
-# Encrypt data using AES encryption
 def encrypt_data(key, data):
+    """Encrypts data using the provided key.
+    
+    Args:
+        key: A bytes-like object representing the encryption key.
+        data: A string to be encrypted.
+
+    Returns:
+        A bytes-like object representing the encrypted data.
+    """
     cipher_suite = Fernet(key)
     encrypted_data = cipher_suite.encrypt(data.encode())
     return encrypted_data
 
-# Decrypt data using AES decryption
 def decrypt_data(key, encrypted_data):
+    """Decrypts data using the provided key.
+    
+    Args:
+        key: A bytes-like object representing the decryption key.
+        encrypted_data: A bytes-like object representing the encrypted data.
+
+    Returns:
+        A string representing the decrypted data.
+    """
     cipher_suite = Fernet(key)
     decrypted_data = cipher_suite.decrypt(encrypted_data).decode()
     return decrypted_data
--- src/utils/input_validation.py
+++ src/utils/input_validation.py
@@ -1,45 +1,32 @@
 import re
+
 def validate_string_input(input_string):
-    if isinstance(input_string, str) and len(input_string) > 0:
-        return True
-    else:
-        return False
+    """Validates if the given input is a non-empty string."""
+    return isinstance(input_string, str) and bool(input_string.strip())
 
 def validate_email_format(email):
+    """Validates if the given email matches the email format."""
     email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    if re.match(email_regex, email):
-        return True
-    else:
-        return False
+    return re.match(email_regex, email) is not None
 
 def validate_integer_input(input_integer):
-    if isinstance(input_integer, int):
-        return True
-    else:
-        return False
+    """Validates if the given input is an integer."""
+    return isinstance(input_integer, int)
 
 def validate_date_format(date):
+    """Validates if the given date matches the YYYY-MM-DD format."""
     date_regex = r'^\d{4}-\d{2}-\d{2}$'
-    if re.match(date_regex, date):
-        return True
-    else:
-        return False
-    
+    return re.match(date_regex, date) is not None
+
 def validate_phone_number_format(phone):
+    """Validates if the given phone number matches the XXX-XXX-XXXX format."""
     phone_regex = r'^\d{3}-\d{3}-\d{4}$'
-    if re.match(phone_regex, phone):
-        return True
-    else:
-        return False
+    return re.match(phone_regex, phone) is not None
 
 def validate_first_name(first_name):
-    if validate_string_input(first_name):
-        if first_name[0].isupper() and first_name.isalpha():
-            return True
-    return False
+    """Validates if the first name is a non-empty string and starts with an uppercase letter."""
+    return validate_string_input(first_name) and first_name[0].isupper() and first_name.isalpha()
 
 def validate_last_name(last_name):
-    if validate_string_input(last_name):
-        if last_name.replace("-", "").isalpha():
-            return True
-    return False
+    """Validates if the last name is a non-empty string and consists only of alphabetic characters or hyphens."""
+    return validate_string_input(last_name) and all(part.isalpha() for part in last_name.split('-'))
