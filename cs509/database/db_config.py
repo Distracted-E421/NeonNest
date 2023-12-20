f201c49b9826b7140c727d81e23ffcdd8455c155
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2023-12-20 16:31:21 -0600

    removed previous project code

--- cs509/database/db_config.py
+++
@@ -1,9 +0,0 @@
-# Database configuration settings
-DB_CONFIG = {
-    "host": "127.0.0.1",
-    "user": "cs509@local",
-    "password": "cs509pw",
-    "database": "neon_nest_apt",
-    "driver": "mysql",
-    "port": 3306
-}
